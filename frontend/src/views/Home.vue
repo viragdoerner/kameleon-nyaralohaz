e6317424384112d10dc024362be4a4b41c10f8ff
Dörner Virag <mal.dorner.virag@gmail.com>
2021-10-20 00:35:43 +0200

    add reserveApartmentSection to homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,28 +1,37 @@
 <template>
-<div>
-  <c-header></c-header>
-  <c-reserve-section></c-reserve-section>
-  <c-icon-list></c-icon-list>
- <hello-world />
-    <hello-world />
-</div>
-  
+  <div>
+    <c-header></c-header>
+    <c-reserve-section></c-reserve-section>
+    <c-icon-list></c-icon-list>
+    <c-reserve-apartment-section
+      v-for="(apartment, index) in apartments"
+      v-bind:key="apartment.id"
+      v-bind:index="index"
+    ></c-reserve-apartment-section>
+  </div>
 </template>
 
 <script>
-import CHeader from './Header.vue'
-import CReserveSection from './ReserveSection.vue'
-import CIconList from './IconList.vue'
-import HelloWorld from '../components/HelloWorld'
+import CHeader from "./Header.vue";
+import CReserveSection from "./ReserveSection.vue";
+import CReserveApartmentSection from "./ReserveApartmentSection.vue";
+import CIconList from "./IconList.vue";
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      CHeader,
-      CReserveSection,
-      CIconList,
-      HelloWorld
-    },
-  }
+  components: {
+    CHeader,
+    CReserveSection,
+    CIconList,
+    CReserveApartmentSection
+  },
+  data: () => ({
+    apartments: [
+      { name: "Földszinti apartman", id: 1 },
+      { name: "Emeleti apartman", id: 2 },
+      { name: "Harmadik apartman", id: 3 },
+    ],
+  }),
+};
 </script>
---
+++ frontend/src/views/ReserveApartmentSection.vue
@@ -0,0 +1,21 @@
+ <template>
+  <v-container class="d-flex px-16 col-10" fluid>
+    <v-card v-bind:class="{ 'order-2': index%2==0 }" width="50%" class="pa-10 ma-10"> bal - lista </v-card>
+    <v-card v-bind:class="{ 'order-1': index%2==1 }" width="50%" class="pa-10 ma-10">
+      jobb - kép és foglalás {{index}}
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CReserveApartmentSection",
+props: {
+    index: {
+      type: Number,
+      required: true
+    },
+  },
+  components: {},
+};
+</script>
