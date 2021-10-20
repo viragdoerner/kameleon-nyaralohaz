953a226b4c1694fc6c5e96c6d123a6e96cd31fbf
Dörner Virag <mal.dorner.virag@gmail.com>
2021-10-20 10:30:11 +0200

    parallax to RAS.vue

--- frontend/src/views/ReserveApartmentSection.vue
+++ frontend/src/views/ReserveApartmentSection.vue
@@ -6,11 +6,16 @@
       style="min-width: 100px; max-width: 100%"
       class="flex-grow-1 flex-shrink-0"
     >
-      <v-card v-bind:class="{ 'justify-end': index % 2 == 5 }" class="pa-2 d-flex justify-center " color="cbggreen" height="100%">
+      <v-card
+        v-bind:class="{ 'justify-end': index % 2 == 5 }"
+        class="pa-2 d-flex justify-center"
+        color="cbggreen"
+        height="100%"
+      >
         <v-list class="px-16 py-10 align-stretch" color="transparent">
-          <v-list-item v-for="item in items" :key="item.title"  >
+          <v-list-item v-for="item in items" :key="item.title">
             <v-list-item-icon>
-              <v-icon  color="clightgreen"> fa-chevron-circle-right </v-icon>
+              <v-icon color="clightgreen"> fa-chevron-circle-right </v-icon>
             </v-list-item-icon>
             <v-list-item-content>
               <v-list-item-title v-text="item.title"></v-list-item-title>
@@ -19,28 +24,24 @@
         </v-list>
       </v-card>
     </v-col>
-    <v-col
-      v-bind:class="{ 'order-1': index % 2 == 1 }"
-      cols="6"
-      style="min-width: 100px; max-width: 100%"
-      class="flex-grow-1 flex-shrink-0"
-    >
-      <v-card height="100%">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/house.jpg"
-          class="white--text align-end p-0"
-          max-width="100%" max-height="100%"
-        >
-        </v-img>
-
-        <v-overlay absolute color="cgreen">
-          <v-card-title class="text-h3 zabatana white--text">
-            kép és foglalás {{ index }}</v-card-title
-          >
-          <v-btn elevation="2" x-large color="corange" class="ma-2">FOGLALÁS </v-btn>
-          <v-btn elevation="2" x-large color="cyellow" class="ma-2">BŐVEBBEN </v-btn>
-        </v-overlay>
-      </v-card>
+    <v-col v-bind:class="{ 'order-1': index % 2 == 1 }" cols="6">
+      <v-parallax dark height="350" src="https://cdn.vuetifyjs.com/images/cards/house.jpg">
+        <v-row align="center" justify="center">
+          <v-col class="text-center" cols="12">
+            <v-overlay absolute color="cgreen">
+              <v-card-title class="text-h3 zabatana white--text">
+                kép és foglalás {{ index }}</v-card-title
+              >
+              <v-btn elevation="2" x-large color="corange" class="ma-2"
+                >FOGLALÁS
+              </v-btn>
+              <v-btn elevation="2" x-large color="cyellow" class="ma-2"
+                >BŐVEBBEN
+              </v-btn>
+            </v-overlay>
+          </v-col>
+        </v-row>
+      </v-parallax>
     </v-col>
   </v-row>
 </template>
