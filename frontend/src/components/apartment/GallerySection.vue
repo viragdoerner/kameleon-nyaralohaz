52c80b5a3e2f8e233a75fca8d594249633c49ade
Dörner Virag <mal.dorner.virag@gmail.com>
2021-11-22 19:46:20 +0100

    kezdetleges lightgallery

--- frontend/src/components/apartment/GallerySection.vue
+++ frontend/src/components/apartment/GallerySection.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>gallery</div>
+  <div class="py-16 gallery-section">
+    <v-container class=" col-10">
+    <c-gallery></c-gallery>
+  </v-container>
+  </div>
 </template>
 
 <script>
+import CGallery from "../Gallery.vue";
 export default {
   name: "CGallerySection",
+  components: {
+    CGallery,
+  },
   data: () => ({}),
   computed: {},
   mounted() {},
@@ -12,5 +20,8 @@ export default {
 };
 </script>
 
-<style>
+<style  scoped>
+.gallery-section {
+  background-color: white;
+}
 </style>
