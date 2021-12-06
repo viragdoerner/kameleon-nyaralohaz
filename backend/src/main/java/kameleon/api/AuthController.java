d9a37ad86ddb068017f7abbba22e0d02c90098e2
Dörner Virag <mal.dorner.virag@gmail.com>
2021-12-06 18:53:26 +0100

    backend admin signin works

--- backend/src/main/java/kameleon/api/AuthController.java
+++ backend/src/main/java/kameleon/api/AuthController.java
@@ -1,9 +1,17 @@
 package kameleon.api;
 
+import kameleon.dao.RoleRepository;
+import kameleon.dao.UserRepository;
 import kameleon.dto.JwtResponse;
 import kameleon.dto.LoginForm;
+import kameleon.model.Role;
+import kameleon.model.RoleName;
+import kameleon.model.User;
+import kameleon.service.ApartmentService;
+import kameleon.service.FileStorageService;
 import kameleon.service.security.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,10 +22,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
@@ -28,6 +38,12 @@ public class AuthController {
 
     @Autowired
     JwtProvider jwtProvider;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
@@ -42,4 +58,43 @@ public class AuthController {
 
         return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), userDetails.getAuthorities()));
     }
+
+    @PostMapping("/setup/roles")
+    public ResponseEntity<?> setupRoles() {
+
+        //hozzáadom a használt szerepeket
+        roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+        roleRepository.save(new Role(RoleName.ROLE_USER));
+
+        return new ResponseEntity<>("Admin and user roles successfully added!",
+                HttpStatus.OK);
+    }
+    @PostMapping("/setup/admin")
+    public ResponseEntity<?> setupAdmin() {
+
+        //leellenőrzöm hogy létre lett-e már hozva ilyen felhasználó
+        if (userRepository.existsByUsername("sbt-admin")) {
+            return new ResponseEntity<>("Fail -> Username is already taken!",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        //hozzáadom az admint
+        User user = new User("Kameleon Admin", "admin", "admin@kameleon.hu",
+                encoder.encode("kameleonadminpassword"));
+
+        Set<Role> roles = new HashSet<>();
+        Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find. Run POST request /api/auth/setup/roles first!"));
+        roles.add(adminRole);
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find. Run POST request /api/auth/setup/roles first!"));
+        roles.add(userRole);
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return new ResponseEntity<>("Admin user succesfully added!",
+                HttpStatus.OK);
+    }
+
 }
