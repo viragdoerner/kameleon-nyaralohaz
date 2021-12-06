b91c54a53d49a4707e550cddb0cbb41ce23fe320
Dörner Virag <mal.dorner.virag@gmail.com>
2021-12-06 17:00:15 +0100

    uj cover design
     (parallax kép balatonszemesrol)

--- frontend/src/components/home/Header.vue
+++ frontend/src/components/home/Header.vue
@@ -1,14 +1,22 @@
 <template>
-  <v-parallax src="../../assets/bg.png">
+<div>
+  <!-- <v-parallax src="../../assets/bg.png">
     <v-row align="center" class="pa-16 ma-16">
-      <v-col  cols="7">
-        
-          <img src="../../assets/cim_feher.svg" />
-          <h3 class="corange--text">BALATONSZEMES</h3>
-
+      <v-col cols="7">
+        <img src="../../assets/cim_feher.svg" />
+        <h3 class="corange--text">BALATONSZEMES</h3>
       </v-col>
-    </v-row></v-parallax
-  >
+    </v-row>
+  </v-parallax> -->
+  <v-parallax src="../../assets/balatonszemes.jpg" :height="innerHeight">
+    <v-row align="top" class="pa-16 ma-16 mt-10">
+      <v-col cols="7">
+        <img src="../../assets/cim_feher.svg" />
+        <h2 class="corange--text">BALATONSZEMES</h2>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</div>
 </template>
 
 <script>
@@ -17,6 +25,11 @@ export default {
 
   components: {},
   data: () => ({}),
+  computed:{
+    innerHeight: function(){
+      return window.innerHeight;
+    }
+  }
 };
 </script>
 <style scoped>
