15e5f18575a3a5479109fa355df58d9081d3e811
Dörner Virag <mal.dorner.virag@gmail.com>
2022-04-02 09:59:04 +0200

    bookingstepone formatting

--- frontend/src/components/booking/BookingStepOne.vue
+++ frontend/src/components/booking/BookingStepOne.vue
@@ -1,24 +1,33 @@
-
-
 <template>
   <v-card class="mb-12" elevation="0">
-    <v-row class="d-flex justify-space-between">
-      <v-col class="col-5">
+    <v-row class="d-flex justify-space-between align-center">
+      <v-col class="col-4 d-flex flex-column">
         <v-combobox
-          v-model="select"
+          v-model="selected_apartment"
           :items="items"
           label="Apartman"
           solo
+          color="cgreen"
         ></v-combobox>
         <v-checkbox
-          v-model="checkbox"
+          v-model="pet_checkbox"
           label="Hozzuk házi kedvencünket is"
           hint="Plusz költséggel nem jár, de korlátozhatja az elérhető dátumok számát."
           persistent-hint
+          color="cgreen"
         ></v-checkbox>
+        <v-text-field
+          :value="'Fizetendő összeg: '+ price"
+          label="Solo"
+          solo dense
+          readonly
+          class="mt-15"
+          :hint="'Az ' + selected_apartment + 'költsége egy éjszakára '+ price+' Ft'"
+          persistent-hint
+        ></v-text-field>
       </v-col>
-      <v-col >
-        <v-date-picker v-model="dates" range></v-date-picker>
+      <v-col class="d-flex justify-start col-6">
+        <v-date-picker v-model="dates" range color="cgreen"></v-date-picker>
       </v-col>
     </v-row>
   </v-card>
@@ -30,10 +39,11 @@ export default {
   props: [],
   components: {},
   data: () => ({
-    select: "",
+    selected_apartment: "",
     items: ["Programming", "Design", "Vue", "Vuetify"],
-    checkbox: true,
+    pet_checkbox: true,
     dates: ["2019-09-10", "2019-09-20"],
+    price: 0
   }),
   methods: {},
   computed: {
@@ -41,5 +51,8 @@ export default {
       return this.dates.join(" ~ ");
     },
   },
+  mounted(){
+    
+  }
 };
 </script>
