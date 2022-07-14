c67e55292a68165e6701ea34acd987cf8b185b42
Dörner Virag <mal.dorner.virag@gmail.com>
2022-07-14 16:43:57 +0200

    pic test

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     snackbar: {},
-    imgPath: "@/assets/images/apartments/",
+    imgPath: "./img/",
     bookingData: {},
     selectedApartmentId: null,
   },
