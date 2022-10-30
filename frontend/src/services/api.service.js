b01f281d1c6ee99c836f3357288bfab8ba2317d8
Dörner Virag <mal.dorner.virag@gmail.com>
2022-10-30 16:20:57 +0100

    change backend api url

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/';
-//const API_URL = 'http://206.189.111.214:8080/';
+//const API_URL = 'http://localhost:8080/';
+const API_URL = 'http://206.189.111.214:8080/';
 
 class ApiService {
     API_URL = API_URL
