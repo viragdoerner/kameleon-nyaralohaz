40c41bbb104bdc9a6cec17861b8d6c593a2b0c57
Dörner Virag <mal.dorner.virag@gmail.com>
2022-11-24 12:22:59 +0100

    switch to sessionStorage

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -1,5 +1,5 @@
 export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
+    let user = JSON.parse(sessionStorage.getItem('user'));
     if (user && user.accessToken) {
       return { Authorization: 'Bearer ' + user.accessToken };
     } else {
